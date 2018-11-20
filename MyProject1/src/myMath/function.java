dfdfe7efbc724a91a716c382291f6a0a340bda91
djtov <djtov@DESKTOP-T8J0HE0>
2018-11-20 20:25:35 +0200

    This interface represents a simple function of type y=f(x), where both y and x are real numbers.

---
+++ MyProject1/src/myMath/function.java
@@ -0,0 +1,8 @@
+package myMath;
+
+/** This interface represents a simple function of type y=f(x), where both y and x are real numbers.
+ * @author David Tover
+**/
+public interface function {
+	public double f(double x);
+}
